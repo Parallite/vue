72149cc553672a72979ab10228a3a76015b3eaef
Sergei <sergeylobodanov@gmail.com>
2022-04-22 15:10:16 +0300

    hw-3

--- fp/src/App.vue
+++ fp/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to my Calc"/>
+    <HomeView />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from "./components/HomeView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HomeView,
+  },
+};
 </script>
 
 <style>
