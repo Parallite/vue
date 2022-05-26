f15ee4e6c545d0eeb65bb68507cc74beefe2a660
Sergei <sergeylobodanov@gmail.com>
2022-05-26 20:48:02 +0300

    added Doughnut hw-8

--- fp/src/views/DashboardView.vue
+++ fp/src/views/DashboardView.vue
@@ -15,20 +15,40 @@
         </v-dialog>
         <PaymentsDisplay :items="paymentsList" @editModeOn="editModeOn" />
       </v-col>
-      <v-col>DIAGRAMM</v-col>
+      <v-col>
+        <template>
+          <DoughnutChart :chartDataInfo="chartDataInfo" />
+        </template>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
 import PaymentsDisplay from "@/components/PaymentsDisplay.vue";
 import AddPaymentForm from "@/components/AddPaymentForm.vue";
+import DoughnutChart from "@/components/DoughnutChart.vue";
 
 export default {
   name: "DashboardView",
-  components: { PaymentsDisplay, AddPaymentForm },
+  components: { PaymentsDisplay, AddPaymentForm, DoughnutChart },
   data() {
     return {
       dialog: false,
+      chartDataInfo: {
+        labels: [],
+        datasets: [
+          {
+            backgroundColor: [
+              "#41B883",
+              "#E46651",
+              "#00D8FF",
+              "#DD1B16",
+              "ffa500",
+            ],
+            data: [],
+          },
+        ],
+      },
     };
   },
   methods: {
@@ -41,6 +61,44 @@ export default {
   computed: {
     paymentsList() {
       return this.$store.getters.getPaymentList;
+    },
+    categoryListDoughnut() {
+      const actualPaymentList = this.$store.getters.getPaymentList;
+      const actualCategoryList = [];
+      for (let i = 0; i < actualPaymentList.length; i++) {
+        if (!actualCategoryList.includes(actualPaymentList[i].category)) {
+          actualCategoryList.push(actualPaymentList[i].category);
+        }
+      }
+      return actualCategoryList;
+    },
+    getCategorySumDoughnut() {
+      const actualPaymentList = this.$store.getters.getPaymentList;
+      const arrOfCategorySum = [];
+      const categoryList = this.chartDataInfo.labels;
+      for (let category of categoryList) {
+        const filteredList = actualPaymentList.filter(
+          (item) => item.category == category
+        );
+        let sum = filteredList.reduce((res, cur) => res + cur.value, 0);
+        arrOfCategorySum.push(sum);
+      }
+      return arrOfCategorySum;
+    },
+  },
+  watch: {
+    categoryListDoughnut() {
+      this.chartDataInfo.labels.push(this.categoryListDoughnut);
+      if (this.categoryListDoughnut.length === 0) {
+        this.chartDataInfo.labels = [];
+        this.chartDataInfo.datasets.backgroundColor = [];
+        this.chartDataInfo.datasets.data = [];
+      } else {
+        this.chartDataInfo.labels = this.categoryListDoughnut;
+      }
+    },
+    getCategorySumDoughnut() {
+      this.chartDataInfo.datasets[0].data = this.getCategorySumDoughnut;
     },
   },
   async mounted() {
